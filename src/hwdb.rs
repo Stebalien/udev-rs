af2694f8289304a98ecc73f307f99da7a754ec01
Steven Allen <steven@stebalien.com>
2014-10-20 22:56:39 -0700

    Begin documenting.

--- src/hwdb.rs
+++ src/hwdb.rs
@@ -22,6 +22,11 @@ pub unsafe fn hwdb(udev: &Udev, hwdb: libudev_c::udev_hwdb) -> Hwdb {
 }
 
 impl<'u> Hwdb<'u> {
+    /// Query the hardware database.
+    ///
+    /// # Note
+    ///
+    /// Only one query can exist at a time.
     pub fn query<'s>(&'s mut self, modalias: &str) -> HwdbQuery<'s, 'u> {
         // HACK: take reference here because we can't reference self.hwdb inside the closure.
         let entry = modalias.with_c_str(|modalias| {
@@ -36,6 +41,7 @@ impl<'u> Hwdb<'u> {
 }
 
 impl<'u> HwdbQuery<'u, 'u> {
+    /// Iterate over the properties returned by this query.
     pub fn iter(&self) -> iter::Map<(&Hwdb, &str, Option<&str>),(&str, &str),UdevIterator<Hwdb>> {
         unsafe { iterator::udev_iterator(self.hwdb, self.entry) }.map(|(_, k, v)| (k, v.unwrap()))
     }
--- src/udev.rs
+++ src/udev.rs
@@ -21,6 +21,7 @@ pub struct Udev {
 }
 
 impl Udev {
+    /// Create a new udev handle.
     pub fn new() -> Udev {
         let udev = unsafe { libudev_c::udev_new() };
         // I don't care about errno. NULL == oom.
@@ -53,14 +54,45 @@ impl Udev {
         Ok(unsafe { monitor::monitor(self, monitor) })
     }
 
+    /// Monitor udev events.
+    ///
+    /// # Error
+    ///
+    /// This will return an error if you're running in an environment without access to netlink.
     pub fn monitor(&self) -> Result<Monitor, IoError> {
         self.create_monitor("udev")
     }
 
+    /// Monitor kernel events.
+    ///
+    /// # Error
+    ///
+    /// This method will return an error if you're running in an environment without access to
+    /// netlink.
+    ///
+    /// # Safety Notes
+    ///
+    /// This method is marked unsafe due to the following warning found in libudev:
+    ///
+    /// > Applications should usually not connect directly to the
+    /// > "kernel" events, because the devices might not be useable
+    /// > at that time, before udev has configured them, and created
+    /// > device nodes. Accessing devices at the same time as udev,
+    /// > might result in unpredictable behavior. The "udev" events
+    /// > are sent out after udev has finished its event processing,
+    /// > all rules have been processed, and needed device nodes are
+    /// > created.
     pub unsafe fn monitor_kernel(&self) -> Result<Monitor, IoError> {
         self.create_monitor("kernel")
     }
 
+    /// Create a new hardware database handle.
+    ///
+    /// # Error
+    ///
+    /// On error, this method will return either Err(errno) or Err(0). Err(errno) indicates a
+    /// problem reading the hardware database and Err(0) indicates that the hardware database is
+    /// corrupt.
     pub fn hwdb(&self) -> Result<Hwdb, i32> {
         match util::check_errno(|| unsafe {
             libudev_c::udev_hwdb_new(self.udev)
@@ -72,6 +104,7 @@ impl Udev {
         }
     }
 
+    /// Lookup a device by sys path.
     pub fn device(&self, path: &Path) -> Option<Device> {
         match path.with_c_str(|path| util::check_errno(|| unsafe {
             libudev_c::udev_device_new_from_syspath(self.udev, path)
@@ -81,6 +114,7 @@ impl Udev {
         }
     }
 
+    /// Lookup a device by device type and device number.
     pub fn device_from_devnum(&self, ty: DeviceType, devnum: Devnum) -> Option<Device> {
         match util::check_errno(|| unsafe {
             libudev_c::udev_device_new_from_devnum(self.udev, ty.to_char(), devnum)
@@ -89,6 +123,8 @@ impl Udev {
             _ => None
         }
     }
+
+    /// Lookup a device by subsystem and sysname
     pub fn device_from_subsystem_sysname(&self, subsystem: &str, sysname: &str) -> Option<Device> {
         match subsystem.with_c_str(|subsystem| sysname.with_c_str(|sysname| util::check_errno(|| unsafe {
             libudev_c::udev_device_new_from_subsystem_sysname(self.udev, subsystem, sysname)
@@ -98,6 +134,7 @@ impl Udev {
         }
     }
 
+    /// Create a device enumerator.
     pub fn enumerator(&self) -> Enumerator {
         unsafe {
             enumerator::enumerator(
