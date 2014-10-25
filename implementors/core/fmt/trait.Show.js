5cc8c7aa2533f9aa3c5d2bb4c014458d46f45bf5
Steven Allen <steven@stebalien.com>
2014-10-24 22:52:44 -0700

    Add API documentation.

---
+++ implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['udev'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'u&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='udev/struct.Device.html' title='udev::Device'>Device</a>&lt;'u&gt;","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='enum' href='udev/enum.Action.html' title='udev::Action'>Action</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='udev/struct.Event.html' title='udev::Event'>Event</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['udev'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='udev/struct.Udev.html' title='udev::Udev'>Udev</a>","<a class='stability Unmarked' title='No stability level'></a>impl&lt;'u&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='udev/struct.Hwdb.html' title='udev::Hwdb'>Hwdb</a>&lt;'u&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;'u&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='udev/struct.Device.html' title='udev::Device'>Device</a>&lt;'u&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;'p&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='udev/struct.Enumerator.html' title='udev::Enumerator'>Enumerator</a>&lt;'p&gt;","<a class='stability Unmarked' title='No stability level'></a>impl&lt;'u&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='udev/struct.Monitor.html' title='udev::Monitor'>Monitor</a>&lt;'u&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
