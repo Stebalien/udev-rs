f72a6e6ea39e0eb43a6b728bdd37d96de849d725
Steven Allen <steven@stebalien.com>
2014-11-06 23:17:39 -0800

    Update documentation.

--- implementors/std/from_str/trait.FromStr.js
+++ implementors/std/from_str/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['udev'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/from_str/trait.FromStr.html' title='std::from_str::FromStr'>FromStr</a> for <a class='enum' href='udev/enum.Action.html' title='udev::Action'>Action</a>",];
+implementors['udev'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/from_str/trait.FromStr.html' title='std::from_str::FromStr'>FromStr</a> for <a class='enum' href='udev/monitor/enum.Action.html' title='udev::monitor::Action'>Action</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
